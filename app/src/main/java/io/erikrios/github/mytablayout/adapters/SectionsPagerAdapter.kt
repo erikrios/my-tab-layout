cb4400a4539c7fae81a7fe57a00107df57997041
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-01 19:45:13 +0700

    Change the override method return value

--- app/src/main/java/io/erikrios/github/mytablayout/adapters/SectionsPagerAdapter.kt
+++ app/src/main/java/io/erikrios/github/mytablayout/adapters/SectionsPagerAdapter.kt
@@ -6,25 +6,19 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import io.erikrios.github.mytablayout.HomeFragment
-import io.erikrios.github.mytablayout.ProfileFragment
 import io.erikrios.github.mytablayout.R
 
 class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) :
     FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     @StringRes
-    private val TAB_TITLES = intArrayOf(R.string.tab_text_1, R.string.tab_text_2)
+    private val TAB_TITLES =
+        intArrayOf(R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3)
 
-    override fun getCount(): Int = 2
+    override fun getCount(): Int = 3
 
-    override fun getItem(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when (position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment = ProfileFragment()
-        }
-        return fragment as Fragment
-    }
+    override fun getItem(position: Int): Fragment = HomeFragment.newInstance(position + 1)
+
 
     override fun getPageTitle(position: Int): CharSequence? =
         mContext.resources.getString(TAB_TITLES[position])
