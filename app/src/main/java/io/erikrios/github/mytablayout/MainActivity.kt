771600f29904579b427c0eebc1dd5150e6ae8f89
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-01 19:29:12 +0700

    Implements the view pager and tab layout

--- app/src/main/java/io/erikrios/github/mytablayout/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/mytablayout/MainActivity.kt
@@ -2,10 +2,21 @@ package io.erikrios.github.mytablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import io.erikrios.github.mytablayout.adapters.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sectionPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+
+        supportActionBar?.elevation = 0f
     }
 }
